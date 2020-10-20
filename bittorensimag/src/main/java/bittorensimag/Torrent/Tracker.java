aa537c6f96a464d437bfd2b17e4a19ae56fa2046
William Goulois <william.goulois@grenoble-inp.org>
2020-10-20 14:02:03 +0200

    Tracker finished !

---
+++ bittorensimag/src/main/java/bittorensimag/Torrent/Tracker.java
@@ -0,0 +1,134 @@
+package bittorensimag.Torrent;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.Random;
+// import java.util.Scanner;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import be.adaxisoft.bencode.BDecoder;
+import be.adaxisoft.bencode.BEncodedValue;
+import be.adaxisoft.bencode.BEncoder;
+import be.adaxisoft.bencode.InvalidBEncodingException;
+// import bittorensimag.Util.Util;
+
+public class Tracker {
+    Torrent torrent;
+    private String url;
+    private String info_hash;
+    private String peer_id;
+    private int port;
+    private int uploaded;
+    private int downloaded;
+    private int left;
+    // private int numwant;
+    private int compact;
+    private String event;
+
+    private String query;
+
+    private Map<String, BEncodedValue> answer;
+
+    public Tracker(Torrent torrent) throws UnsupportedEncodingException {
+        this.torrent = torrent;
+        this.url = (String) this.torrent.getMetadata().get("announce");
+        try {
+            this.hashInfo();
+        } catch (InvalidBEncodingException e) {
+            e.printStackTrace();
+        }
+        this.peer_id = "-" + "BE" + "0001" + "-" + generateRandomAlphanumeric(12);
+        // TODO need to try ports available from 6881 to 6889
+        this.port = 6881;
+        this.downloaded = 0;
+        this.uploaded = 0;
+        // TODO how to calculate numwant ? Not equal to length in byte of the file ???
+        this.left = 806512;
+        // this.numwant = 50;
+        this.compact = 1;
+        this.event = "started";
+        this.query = "";
+        this.generateUrl();
+    }
+
+    private void hashInfo() throws InvalidBEncodingException {
+        Map<String, BEncodedValue> info = this.torrent.getInfo();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try {
+            BEncoder.encode(info, baos);
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("SHA-1");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        md.update(baos.toByteArray());
+        // String s = Util.bytesToHex(md.digest()); // to test sha1
+        String s = new String(md.digest(), StandardCharsets.ISO_8859_1);
+        this.info_hash = s;
+    }
+
+    private static String generateRandomAlphanumeric(int targetStringLength) {
+        int leftLimit = 48; // number '0'
+        int rightLimit = 122; // letter 'z'
+        Random random = new Random();
+
+        String generatedString = random.ints(leftLimit, rightLimit + 1)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97)).limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
+        return generatedString;
+    }
+
+    private void generateUrl() throws UnsupportedEncodingException {
+        try {
+            System.out.println("Info hash is : " + this.info_hash);
+            this.query += "info_hash=" + URLEncoder.encode(this.info_hash, "ISO_8859_1") + "&" + "peer_id="
+                    + URLEncoder.encode(this.peer_id, "UTF-8") + "&" + "port=" + this.port + "&" + "uploaded="
+                    + this.uploaded + "&" + "downloaded=" + this.downloaded + "&" + "left=" + this.left + "&"
+                    + "compact=" + this.compact + "&" + "event=" + this.event;
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getRequest() {
+        URLConnection connection;
+        try {
+            System.out.println(this.query);
+            connection = new URL(this.url + "?" + this.query).openConnection();
+
+            connection.setRequestProperty("Accept-Charset", StandardCharsets.UTF_8.toString());
+            InputStream stream = connection.getInputStream();
+            // Debug to output answer of the tracker
+            // try (Scanner scanner = new Scanner(stream)) {
+            // String output = scanner.useDelimiter("\\A").next();
+            // System.out.println(output);
+            // }
+            this.decodeAnswer(stream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void decodeAnswer(InputStream stream) {
+        BDecoder reader = new BDecoder(stream);
+        try {
+            this.answer = reader.decodeMap().getMap();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
