ebff859e6e1910914d3b4251ca97afe46067150a
Aiman Wahdi Mohd Imran <aimanwahdi@Aimans-MacBook-Pro.local>
2020-11-02 12:51:35 +0100

    ajout des codeurs pour tous les messages. A TESTER

--- bittorensimag/src/main/java/bittorensimag/BitfieldCoder.java
+++ bittorensimag/src/main/java/bittorensimag/BitfieldCoder.java
@@ -1,34 +1,35 @@
 package bittorensimag;
 
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
-public class MsgBinCoder{ //coder pour choke, unchoke, uninterested, interested
-	
-	public byte[] toWire(Msg msg) throws IOException {		//converts the message to a sequence of byte
+public class BitfieldCoder {
+	public byte[] toWire(MsgBitfield msg) throws IOException {		//converts the message to a sequence of byte
 		ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
 		DataOutputStream out = new DataOutputStream(byteStream);
 		
-		if(msg.getMsgType()>=0 && msg.getMsgType()<4) {
-			out.writeInt(msg.getMsgLength());
-			out.writeByte(msg.getMsgType());
-		} 
+		out.write(msg.getMsgLength());
+		out.writeByte(msg.getMsgType());
+		out.write(msg.getBitfieldDATA());
 		
 		out.flush();
 		byte[] data = byteStream.toByteArray();
 	    return data;
 	}
 	
-	public Msg fromWire(byte[] input) throws IOException{	//parses a given sequence of bytes
+	public MsgBitfield fromWire(byte[] input) throws IOException{	//parses a given sequence of bytes
 		ByteArrayInputStream bs = new ByteArrayInputStream(input);
 	    DataInputStream in = new DataInputStream(bs);
 	    
 	    int length = in.readInt();
 	    int type = in.readByte();
+	    byte[] data = new byte[length - 1];
+	    in.readFully(data);
 	    
-	    return new Msg(length, type);
+		return new MsgBitfield(length, type, data);
+		
 	}
 }
