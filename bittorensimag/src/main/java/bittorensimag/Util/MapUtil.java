4034cbf1a2282c802b35728e2f0d56da2e4bee24
William Goulois <william.goulois@grenoble-inp.org>
2020-11-08 16:00:36 +0100

    add convert hashmap to util

--- bittorensimag/src/main/java/bittorensimag/Util/MapUtil.java
+++ bittorensimag/src/main/java/bittorensimag/Util/MapUtil.java
@@ -1,11 +1,13 @@
 package bittorensimag.Util;
 
+import java.nio.ByteBuffer;
+import java.util.Iterator;
 import java.util.Map;
 
 import be.adaxisoft.bencode.BEncodedValue;
 import be.adaxisoft.bencode.InvalidBEncodingException;
 
-public class BencodeMap {
+public class MapUtil {
     // Methods to get keys
     public static String getKeyString(Map<String, BEncodedValue> map, String key) throws InvalidBEncodingException {
         try {
@@ -49,21 +51,32 @@ public class BencodeMap {
     public static void fillBencodeMapString(Map<String, BEncodedValue> sourceMap, Map<String, Object> destMap,
             String[] keyStrings) throws InvalidBEncodingException {
         for (String key : keyStrings) {
-            destMap.put(key, BencodeMap.getKeyString(sourceMap, key));
+            destMap.put(key, MapUtil.getKeyString(sourceMap, key));
         }
     }
 
     public static void fillBencodeMapInt(Map<String, BEncodedValue> sourceMap, Map<String, Object> destMap,
             String[] keyStrings) throws InvalidBEncodingException {
         for (String key : keyStrings) {
-            destMap.put(key, BencodeMap.getKeyInt(sourceMap, key));
+            destMap.put(key, MapUtil.getKeyInt(sourceMap, key));
         }
     }
 
     public static void fillBencodeMapBytes(Map<String, BEncodedValue> sourceMap, Map<String, Object> destMap,
             String[] keyStrings) throws InvalidBEncodingException {
         for (String key : keyStrings) {
-            destMap.put(key, BencodeMap.getKeyByte(sourceMap, key));
+            destMap.put(key, MapUtil.getKeyByte(sourceMap, key));
         }
+    }
+
+    public static byte[] convertHashMapToByteArray(int lengthOutput, Map<Integer, byte[]> dataMap) {
+        ByteBuffer buffer = ByteBuffer.allocate(lengthOutput);
+        Iterator<Map.Entry<Integer, byte[]>> iterator = dataMap.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<Integer, byte[]> mapEntry = (Map.Entry<Integer, byte[]>) iterator.next();
+            System.out.println("clé: " + mapEntry.getKey());
+            buffer.put((byte[]) mapEntry.getValue());
+        }
+        return buffer.array();
     }
 }
