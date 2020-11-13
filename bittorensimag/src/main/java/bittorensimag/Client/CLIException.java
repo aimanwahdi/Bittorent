5116ed5ef12e83f4cc657f3c15242427713bbeef
William Goulois <william.goulois@grenoble-inp.org>
2020-11-14 00:49:35 +0100

    added CLIException class

---
+++ bittorensimag/src/main/java/bittorensimag/Client/CLIException.java
@@ -0,0 +1,16 @@
+package bittorensimag.Client;
+
+/**
+ * Exception raised when the command-line options are incorrect.
+ *
+ * @author gouloisw
+ * @date 14/11/2020
+ */
+public class CLIException extends Exception {
+
+    private static final long serialVersionUID = -7687556758547705280L;
+
+    public CLIException(final String message) {
+        super(message);
+    }
+}
