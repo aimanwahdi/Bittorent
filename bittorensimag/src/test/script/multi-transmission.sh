2ef31c2efc47d3d070d543ddf779db0bb9fed526
William Goulois <william.goulois@grenoble-inp.org>
2020-11-15 01:13:48 +0100

    clean now deletes all torrents and patch seeder

--- bittorensimag/src/test/script/multi-transmission.sh
+++ bittorensimag/src/test/script/multi-transmission.sh
@@ -2,6 +2,9 @@
 
 # Exemple de script de lancement de plusieurs clients transmissions
 
+# Start by cleaning everything
+./src/test/script/clean.sh
+
 # My directory structure
 # ..
 # ├── opentracker
@@ -19,9 +22,10 @@
 
 torrent=./src/test/exampleTorrents/Aigle.jpg.torrent
 file=./src/test/exampleFiles/Aigle.jpg
-fileFolder=../exampleFiles
+fileFolder=$HOME/Downloads
 
-./src/test/script/clean.sh
+# Copy file to fileFolder to be detected by transmission
+cp $file $fileFolder
 
 # Start of opentracker
 ../../opentracker/opentracker.debug -i 127.0.0.1 -p 6969 </dev/null &>/dev/null &
@@ -34,11 +38,20 @@ seeder_number=3
 for i in $(seq 1 ${seeder_number});do
     port=$((9090 + $i))
     echo "Starting seeder $i on port $port"
+
+	# Start daemon
 	transmission-daemon -p $port -w $fileFolder
 	sleep 1
-	# limitation possible de l'upload à 1Mo/s via -u 1M
-	transmission-remote $port -a $torrent
-
+	# Add torrent
+	transmission-remote $port -a $torrent --find $fileFolder
+	sleep 1
+	# Verify local data
+	transmission-remote $port -t all -v
+	sleep 1 
+	# Start torrent
+	transmission-remote $port -t all -s 
+	sleep 1
+	
     urls+=" localhost:$port"
 done
 
