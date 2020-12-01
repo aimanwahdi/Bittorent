dc34327d2d82fc56ac52c33c5a2eb46e0547ad51
Aiman Wahdi Mohd Imran <aimanwahdi@Aimans-MacBook-Pro.local>
2020-12-01 22:58:40 +0100

    Ajout d'une classe PieceManager pour gerer l'etat d'avancement de telechargement et retourne le prochaine piece à telecharger

---
+++ bittorensimag/src/main/java/bittorensimag/Util/PieceManager.java
@@ -0,0 +1,37 @@
+package bittorensimag.Util;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class PieceManager {
+	private HashMap<Integer, ArrayList<String>> pieceMap; //Clé : Piece Index, Valeur : List of peers that contains this piece
+	private ArrayList<Boolean> downloaded;
+	private ArrayList<Boolean> requestSent;
+	private int numOfPiece;
+	
+	
+	public PieceManager(HashMap<Integer, ArrayList<String>> pieceMap, ArrayList<Boolean> downloaded, ArrayList<Boolean> requestSent, int numOfPiece) {
+		super();
+		this.pieceMap = pieceMap;
+		this.downloaded = downloaded;
+		this.requestSent = requestSent;
+		this.numOfPiece = numOfPiece;
+	}
+	
+	public void requestSent(int pieceRequested) {
+		requestSent.set(pieceRequested, true);
+	}
+	
+	public int nextPieceToRequest(int pieceReceived, String currentPeer) {
+		downloaded.set(pieceReceived, true);
+		int nextPiece = 0;
+		while ((nextPiece < numOfPiece) && (requestSent.get(nextPiece) == true || !pieceMap.get(nextPiece).contains(currentPeer)) ) {
+			nextPiece += 1;
+		}
+		return nextPiece;
+		
+	}
+	
+	
+
+}
