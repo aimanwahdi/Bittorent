b6302e52a1736f8f12b811b4a0e1d9e510656fb5
William Goulois <william.goulois@grenoble-inp.org>
2020-12-06 19:33:28 +0100

    ListUtils intersection and union

---
+++ bittorensimag/src/main/java/bittorensimag/Util/ListUtils.java
@@ -0,0 +1,52 @@
+package bittorensimag.Util;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class ListUtils {
+
+    public static <T> List<T> union(List<T> list1, List<T> list2) {
+        Set<T> set = new HashSet<T>();
+
+        set.addAll(list1);
+        set.addAll(list2);
+
+        return new ArrayList<T>(set);
+    }
+
+    public static <T> List<T> intersection(List<T> list1, List<T> list2) {
+        List<T> list = new ArrayList<T>();
+
+        for (T t : list1) {
+            if (list2.contains(t)) {
+                list.add(t);
+            }
+        }
+
+        return list;
+    }
+
+    // remove list2 elements of list1
+    public static <T> List<T> substraction(List<T> list1, List<T> list2) {
+        List<T> list = new ArrayList<T>();
+
+        for (T t : list1) {
+            if (!list2.contains(t)) {
+                list.add(t);
+            }
+        }
+
+        return list;
+    }
+
+    // private void exemple() {
+    // List<String> list1 = new ArrayList<String>(Arrays.asList("A", "B", "C"));
+    // List<String> list2 = new ArrayList<String>(Arrays.asList("B", "C", "D", "E",
+    // "F"));
+
+    // System.out.println(ListUtils.intersection(list1, list2));
+    // System.out.println(ListUtils.union(list1, list2));
+    // }
+}
--- bittorensimag/src/main/java/bittorensimag/Util/Util.java
+++ bittorensimag/src/main/java/bittorensimag/Util/Util.java
@@ -3,7 +3,6 @@ package bittorensimag.Util;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map.Entry;
 import java.util.Random;
 
