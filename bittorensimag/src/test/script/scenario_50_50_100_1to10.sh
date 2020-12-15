f489976234cc50ef7411e7632bc3e28e7025deca
William Goulois <william.goulois@grenoble-inp.org>
2020-12-15 10:53:29 +0100

    piece_1_to_10 from prof

---
+++ bittorensimag/src/test/script/scenario_50_50_100_1to10.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+# If we're not in the root of the Maven project, then find it using
+# this script's name:
+if ! [ -r pom.xml ]; then
+    cd "$(dirname "$0")"/../../../
+fi
+# Exemple de script de lancement de plusieurs clients transmissions
+
+# Start by cleaning clients
+#./src/test/script/cleanClients.sh
+
+# My directory structure
+# ..
+# ├── opentracker
+# |	└── opentracker.debug
+# |	..
+# |	├── src
+# |	│   └── test
+# |	│       ├── exampleFiles
+# |	│       │   └── Aigle.jpg
+# |	│       ├── exampleTorrents
+# |	│       │   └── Aigle.jpg.torrent
+# |	│       └── script
+# |	│           ├── clean.sh
+# |	│           └── multi-transmission.sh
+
+torrent=./src/test/exampleTorrents/piece_1_to_10.torrent
+file=./src/test/exampleFiles/piece_1_to_10
+fileBegin=./src/test/Piece1to5/piece_1_to_10
+fileEnd=./src/test/Piece6to10/piece_1_to_10
+fileFolder=$HOME/Downloads
+
+for i in $(seq 1 3);do
+    echo "Creating folder for seeder $i"
+	mkdir $fileFolder/aria2c_$i
+done
+
+# Clean to begin as leecher 0%
+# rm ./src/test/outputFolder/Aigle.jpg
+
+echo "Copying data in folders"
+cp $fileBegin $fileFolder/aria2c_1
+cp $fileEnd $fileFolder/aria2c_2
+cp $file $fileFolder/aria2c_3
+
+for i in $(seq 1 3);do
+    port=$((2000 + $i))
+	rpcPort=$((6800 + $i))
+    echo "Starting seeder $i on port $port"
+
+	downloadFolder=$fileFolder/aria2c_$i
+
+	aria2c --check-integrity=true --max-upload-limit=10K --disable-ipv6=true --enable-dht=false --enable-dht6=false --enable-peer-exchange=false --enable-rpc --rpc-listen-all --rpc-listen-port=$rpcPort --seed-ratio=0.0 --listen-port $port -V -d $downloadFolder $torrent &>./src/test/logs/aria2c_$i.log &
+	sleep 1
+done
